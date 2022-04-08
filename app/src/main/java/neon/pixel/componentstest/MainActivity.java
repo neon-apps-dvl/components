8d72646b9d6860abed5a945d0af23f6024c96a1a
max <pixel.apps.dvl@gmai.com>
2022-04-08 18:38:45 +0100

    Commit

--- app/src/main/java/neon/pixel/componentstest/MainActivity.java
+++ app/src/main/java/neon/pixel/componentstest/MainActivity.java
@@ -1,13 +1,23 @@
 package neon.pixel.componentstest;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+
+
     }
 }
